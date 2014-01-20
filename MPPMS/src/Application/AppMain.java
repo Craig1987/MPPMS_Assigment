fcea49c68ade66272d0a2b98d11ac87e1e5a8546
Kirstyvox <kirstyvox@aol.com>
2014-01-20 12:45:25 +0000

    Changed LookAndFeel to system

--- MPPMS/src/Application/AppMain.java
+++ MPPMS/src/Application/AppMain.java
@@ -1,8 +1,19 @@
 package Application;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class AppMain
 {
     public static void main(String[] args) {
+        try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) 
+        {
+            //Default look and feel is 'Metal'
+        }
+        
         AppController controller = new AppController();
         controller.showLogin();
     }
